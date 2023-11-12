7e2d8e909b8bd33302ab42bac96e5489cd8fb371
hafizaryan <hafizaryansiregar@proton.me>
2023-11-12 08:10:31 +0700

    Initial commit

---
+++ application/views/home/kontak.php
@@ -0,0 +1,42 @@
+ <section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url(); ?>assets/User/images/bg_2.jpg');">
+   <div class="overlay"></div>
+   <div class="container">
+     <div class="row no-gutters slider-text align-items-center justify-content-center">
+       <div class="col-md-9 ftco-animate text-center">
+         <h1 class="mb-2 bread">Kontak Kami</h1>
+         <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Kontak <i class="ion-ios-arrow-forward"></i></span></p>
+       </div>
+     </div>
+   </div>
+ </section>
+
+ <section class="ftco-section contact-section">
+   <div class="container">
+     <div class="row d-flex mb-5 contact-info">
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Alamat</h3>
+           <p>Pangkalan Kerinci Kota, Kec. Pangkalan Kerinci, Kabupaten Pelalawan, Riau 28654</p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Kontak Kami</h3>
+           <p><a href="https://wa.me/6281364477910"> 081364477910</a></p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Email Address</h3>
+           <p><a href="mailto:atasee@gmail.com.com">atasee@gmail.com</a></p>
+         </div>
+       </div>
+       <div class="col-md-3 d-flex">
+         <div class="bg-light align-self-stretch box p-4 text-center">
+           <h3 class="mb-4">Website</h3>
+           <p><a href="#">Atasee.com</a></p>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
---
+++ application/views/home/tentang.php
@@ -0,0 +1,140 @@
+<section class="hero-wrap hero-wrap-2" style="background-image: url('<?= base_url(); ?>assets/User/images/bg_2.jpg');">
+	<div class="overlay"></div>
+	<div class="container">
+		<div class="row no-gutters slider-text align-items-center justify-content-center">
+			<div class="col-md-9 ftco-animate text-center">
+				<h1 class="mb-2 bread">Tentang Kami</h1>
+				<p class="breadcrumbs"><span class="mr-2"><a href="#">Home <i class="ion-ios-arrow-forward"></i></a></span> <span>Tentang<i class="ion-ios-arrow-forward"></i></span></p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="ftco-section ftco-no-pt ftc-no-pb">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 order-md-last wrap-about py-5 wrap-about bg-light">
+				<div class="text px-4 ftco-animate">
+					<h2 class="mb-4">Tentang Kami</h2>
+					<p>Selamat datang di Bimbel CV Atasee</h2>
+						<p>Atasee adalah sebuah lembaga pendidikan yang terletak di Pangkalan Kerinci.</p>
+						<p>Lembaga ini berkonsentrasi pada program-program belajar yang ditujukan untuk
+							mengembangkan serta mengoptimalkan potensi kecerdasan siswa. Diharapkan
+							keberadaan Atasee dapat menjadi mitra terbaik bagi orang tua dan sekolah dalam
+							program pengembangan kecerdasan anak.</p>
+					<h2 class="mb-4">Visi Dan Misi</h2>
+					<p>“Menjadi lembaga pendidikan pencetak generasi berprestasi berahlak mulia untuk menghadapi kemajuan dan persaingan dunia akademis dengan tata cara yang terbaik.”</h2>
+
+<p>Menjadi Lembaga Pendidikan Pencetak Generasi Berprestasi. Generesi yang berprestasi  hendaknya memiliki komitment untuk melakukan perbaikan terus menerus secara sistimatis guna mencapai  keunggulan terbaik.
+Berahlak Mulia.Taat Beribadah, Disiplin, Team Work, Komitment Integritas, Kreatif, Gembira, Senyum, Salam Dan Sapa.</h2>
+<p>Menghadapi Kemajuan Dan Persaingan Dunia Akademis.Dapat mengatasi persaingan yang ketat untuk bisa masuk pada jenjang pendidikan yang lebih tinggi.
+Dengan Tata Cara Yang Terbaik. Sistem yang bersinambungan. Masa depan generasi indonesia sanggup, tangguh, siap menghadapi tantangan dan kesulitan yang siap menjawab kebutuhan dan kemajuan zaman.</p>
+				</div>
+			</div>
+			<div class="col-md-7 wrap-about py-5 pr-md-4 ftco-animate">
+				<h2 class="mb-4">Apa yang kami berikan</h2>
+				<p>Untuk Memberikan pelayanan terbaik kami memberikan :</p>
+					<div class="row mt-5">
+						<div class="col-lg-6">
+							<div class="services-2 d-flex">
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-security"></span></div>
+								<div class="text">
+									<h3>Keselamatan</h3>
+									<p>Point ini sangat lah kami jaga karna keselamatan adalah nomor satu dari bimble kami</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="services-2 d-flex">
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-reading"></span></div>
+								<div class="text">
+									<h3>Kategori Kelas</h3>
+									<p> Bimbel Kami Terbagi Dari Beberapa Kategori Kelas yaitu Membaca, Komputer, Bahasa Inggris 
+										Dan Lainnya.</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="services-2 d-flex">
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-diploma"></span></div>
+								<div class="text">
+									<h3>Guru Bersertifikat</h3>
+									<p>Semua Guru yang mengajar bersertifikat resmi</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="services-2 d-flex">
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-education"></span></div>
+								<div class="text">
+									<h3>Ruang Kelas yang Cukup</h3>
+									<p>Terdapat Ruang Kelas Yang Luas Dan Nyaman</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="services-2 d-flex">
+								<div class="icon mt-2 mr-3 d-flex justify-content-center align-items-center"><span class="flaticon-kids"></span></div>
+								<div class="text">
+									<h3>Fasilitas Belajar</h3>
+									<p>Ada fasilitas belajar yang lengkap dan mumpuni dan mampu melengkapi kebutuhan anak anda</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="ftco-section ftco-counter img" id="section-counter" style="background-image: url(<?= base_url(); ?>assets/User/images/bg_4.jpg);" data-stellar-background-ratio="0.5">
+	<div class="container">
+		<div class="row justify-content-center mb-5 pb-2">
+			<div class="col-md-8 text-center heading-section heading-section-black ftco-animate">
+				<h2 class="mb-4"><span>20 Years of</span> Experience</h2>
+			</div>
+		</div>
+		<div class="row d-md-flex align-items-center justify-content-center">
+			<div class="col-lg-10">
+				<div class="row d-md-flex align-items-center">
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_admin; ?>">0</strong>
+								<span>Total Admin</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_pendaftar; ?>">0</strong>
+								<span>Total Pendaftar</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_user; ?>">0</strong>
+								<span>Total Siswa</span>
+							</div>
+						</div>
+					</div>
+					<div class="col-md d-flex justify-content-center counter-wrap ftco-animate">
+						<div class="block-18">
+							<div class="icon"><span class="flaticon-doctor"></span></div>
+							<div class="text">
+								<strong class="number" data-number="<?= $total_kelas; ?>">0</strong>
+								<span>Total Kelas</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
